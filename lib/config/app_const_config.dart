8245a442d8699eb12731b37e85f8ec9809ff5487
Sid <siddhartha@dcpltech.com>
2023-11-06 12:04:10 +0530

    Minor Changes

--- lib/config/app_const_config.dart
+++ lib/config/app_const_config.dart
@@ -1,6 +1,6 @@
 class AppConstConfig {
-  static const String baseUrl = "https://medshopapi.herokuapp.com";
-  //static const String baseUrl = "https://localhost:8080";
+  //static const String baseUrl = "https://medshopapi.herokuapp.com";
+  static const String baseUrl = "http://localhost:8080";
   static const Map<int, String> navBarItem = {
     1: "Product",
     2: "Sales",
